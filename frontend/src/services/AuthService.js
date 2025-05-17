2d74cf9efdca8de962802785b57fb32d62f698e6
KavindaKGD <kavindakgd@gmail.com>
2025-05-17 16:12:57 +0530

    .

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://country-api-bvir.onrender.com/api/auth";
 
 export const loginUser = async (email, password) => {
   try {
